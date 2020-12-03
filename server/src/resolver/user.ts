d3c9733c2f2795b705d8d37436472c28904cc855
nutchapon.h <nutchapon@enres.com>
2020-12-03 15:43:53 +0700

    10-register-error-handling

--- server/src/resolver/user.ts
+++ server/src/resolver/user.ts
@@ -11,6 +11,8 @@ import {
   Query,
 } from "type-graphql";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
+import { update } from "lodash";
 
 @InputType()
 class UsernamePasswordInput {
@@ -78,13 +80,19 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-    const user = await em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
-
+    let user;
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
     } catch (err) {
       if (err.code === "23505") {
         return {
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -4,18 +4,27 @@ import { Box, Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMaps";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
   const [, register] = useRegisterMutation();
+  const router = useRouter();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data.register.user) {
+            //work
+            router.push("/");
+          }
           return response;
         }}
       >
